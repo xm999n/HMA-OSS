4d69a4d677965f7f6c52d18317a26b94d6bf3e0e
frknkrc44 <krc440002@gmail.com>
2026-03-29 15:45:46 +0300

    Improve the non-valid keyboard warning

--- zygote/src/main/java/org/frknkrc44/hma_oss/zygote/hook/ImmHook.kt
+++ zygote/src/main/java/org/frknkrc44/hma_oss/zygote/hook/ImmHook.kt
@@ -285,7 +285,7 @@ class ImmHook(private val service: HMAService) : IFrameworkHook {
     }
 
     private fun warnNotInstalledKeyboard(methodName: String, packageName: String) {
-        logW(TAG, "@$methodName: Spoofing for a not installed keyboard, please install $packageName or spoof for another keyboard by using settings templates to reduce detections")
+        logW(TAG, "@$methodName: (Probably) spoofing for a not installed keyboard, please install $packageName or spoof for another keyboard by using settings templates to reduce detections")
     }
 
     private fun callerIsSpoofed(caller: String) =
